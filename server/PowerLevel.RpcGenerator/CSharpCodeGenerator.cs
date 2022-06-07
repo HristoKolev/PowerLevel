b04f8df6aef319ee1eebb84a084d91d8d54ce01c
Hristo Kolev <hristo.a.kolev@gmail.com>
2022-06-07 08:22:08 +0300

    code generation refactoring

--- server/PowerLevel.RpcGenerator/CSharpCodeGenerator.cs
+++ server/PowerLevel.RpcGenerator/CSharpCodeGenerator.cs
@@ -6,38 +6,39 @@ using Xdxd.DotNet.Rpc;
 
 public static class CSharpCodeGenerator
 {
+    private static string GetMethodName(RpcRequestMetadata metadata)
+    {
+        const string REQUEST_POSTFIX = "request";
+
+        string methodName = metadata.RequestType.Name;
+
+        if (methodName.ToLower().EndsWith(REQUEST_POSTFIX))
+        {
+            return methodName.Remove(methodName.Length - REQUEST_POSTFIX.Length, REQUEST_POSTFIX.Length);
+        }
+
+        return methodName;
+    }
+
     public static string Generate(List<RpcRequestMetadata> metadata)
     {
-        const string FILE_TEMPLATE = @"namespace PowerLevel.Server.Tests;
+        var methods = metadata.Select(x => $"    public virtual Task<Result<{x.ResponseType.Name}>> " +
+                                           $"{GetMethodName(x)}({x.RequestType.Name} request)\n    {{\n    " +
+                                           $"    return this.RpcExecute<{x.RequestType.Name}, {x.ResponseType.Name}>(request);\n    }}");
+
+        return @$"namespace PowerLevel.Server.Tests;
 
 using System.Threading.Tasks;
 using Auth;
 using Xdxd.DotNet.Shared;
 
 public abstract class RpcClient
-{
+{{
     protected abstract Task<Result<TResponse>> RpcExecute<TRequest, TResponse>(TRequest request)
         where TRequest : class where TResponse : class;
 
-{methods}
-}
+{string.Join("\n\n", methods)}
+}}
 ";
-        var methods = metadata.Select(x =>
-        {
-            string methodName = x.RequestType.Name;
-
-            const string REQUEST_POSTFIX = "request";
-
-            if (methodName.ToLower().EndsWith(REQUEST_POSTFIX))
-            {
-                methodName = methodName.Remove(methodName.Length - REQUEST_POSTFIX.Length, REQUEST_POSTFIX.Length);
-            }
-
-            return $"    public virtual Task<Result<{x.ResponseType.Name}>> " +
-                   $"{methodName}({x.RequestType.Name} request)\n    {{\n    " +
-                   $"    return this.RpcExecute<{x.RequestType.Name}, {x.ResponseType.Name}>(request);\n    }}";
-        });
-
-        return FILE_TEMPLATE.Replace("{methods}", string.Join("\n\n", methods));
     }
 }
