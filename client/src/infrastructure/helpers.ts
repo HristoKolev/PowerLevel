04961d26c56bb69764052e33c366a0352155764f
Hristo Kolev <hristo.a.kolev@gmail.com>
2022-06-15 03:18:54 +0300

    removed unused code

--- client/src/infrastructure/helpers.ts
+++ client/src/infrastructure/helpers.ts
@@ -1,7 +1,2 @@
-export interface SelectItem<T = unknown> {
-  value: T;
-  label: string;
-}
-
 export const delay = (interval: number): Promise<void> =>
   new Promise((resolve) => setTimeout(resolve, interval));
