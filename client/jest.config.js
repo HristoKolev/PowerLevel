5665c8fba78da4ddb6ef40d11665e51bc75a2dbf
Hristo Kolev <hristo.a.kolev@gmail.com>
2022-08-06 03:30:31 +0300

    comments

--- client/jest.config.js
+++ client/jest.config.js
@@ -7,10 +7,10 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
-    '!<rootDir>/src/index.tsx',
-    '!<rootDir>/src/service-worker.ts',
-    '!<rootDir>/src/infrastructure/test-utils.tsx',
-    '!<rootDir>/src/rpc/RpcClient.ts',
+    '!<rootDir>/src/index.tsx', // Entrypoint
+    '!<rootDir>/src/service-worker.ts', // Entrypoint
+    '!<rootDir>/src/infrastructure/test-utils.tsx', // Test utilities, not production code
+    '!<rootDir>/src/rpc/RpcClient.ts', // Generated code
   ],
   coverageDirectory: 'coverage',
   coverageThreshold: {
